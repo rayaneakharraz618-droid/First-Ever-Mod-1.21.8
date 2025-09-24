868dc0a35d93fcb6389e1042ca0f034c64f10c30
Rayan <rayaneakharraz618@gmail.com>
2025-09-24 21:57:02 +0100

    hello

---
+++ src/main/java/net/galaxy/testing/TestingClient.java
@@ -0,0 +1,10 @@
+package net.galaxy.testing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/galaxy/testing/TestingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.galaxy.testing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
